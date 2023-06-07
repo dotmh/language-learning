5e8507bd68af01ca5ac0074e0c295eb726758c79
Martin Haynes <martin.haynes@focusrite.com>
2023-06-07 17:42:19 +0100

    add code to get the latest exchange rate

--- go/currency.go
+++ go/currency.go
@@ -1,32 +1,46 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
+	"os"
 )
 
-func makeRequest(url string) string {
+type GBP struct {
+	GBP float64 `json:"GBP"`
+}
+
+type CurrencyData struct {
+	Data GBP `json:"data"`
+}
+
+
+func main() {
+	key := os.Getenv("FREE_CURRENCY_API_KEY")
+	urlTemplate := "https://api.freecurrencyapi.com/v1/latest?apikey=%s&currencies=GBP&base_currency=JPY"
+
+	if (key == "") { 
+		log.Fatalln("Please set you Free Currency API Key")
+	}
+
+	url := fmt.Sprintf(urlTemplate, key)
+
 	res, err := http.Get(url);
 
 	if err != nil {
 		log.Fatalln("Couldn't get request");
 	}
 
-	body, err := io.ReadAll(res.Body)
+	defer res.Body.Close()
 
-	if err != nil {
-		log.Fatalln("Couldn't read the body");
+	var jsonResponse CurrencyData
+
+	if err := json.NewDecoder(res.Body).Decode(&jsonResponse); err != nil {
+		log.Println(err)
+		log.Fatalln("Can not convert to JSON")
 	}
 
-	bodyText := string(body)
-	return bodyText;
-}
-
-func main() {
-
-	learn := makeRequest("https://learn.api.dotmh.dev");
-
-	fmt.Println(learn);
+	fmt.Println(jsonResponse.Data.GBP)
 }
